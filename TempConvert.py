7ef31a98f9b640d55d39919883dfe5cfa4bc2e38
Shaobo <bobyu1993@zju.edu.cn>
2019-03-30 18:55:38 +0800

    new example exercise

--- TempConvert.py
+++ TempConvert.py
@@ -5,7 +5,7 @@ if val[-1] in ['C','c']:
     f = 1.8 * float(val[0:-1]) + 32
     print("转换后的温度为： %.2fF"%f)
 elif val[-1] in ['F','f']:
-    c = (float(val[0,-1])-32)/1.8
+    c = (float(val[0:-1])-32)/1.8
     print("转换后的温度为： %.2fC"%c)
 else:
     print("输入有误")
